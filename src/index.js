6fdcc01aa4c6fbbd30d9a76266fb1dbf89fd6c4e
jocsel <jocsel52@gmail.com>
2021-07-23 16:09:03 -0600

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import {saludar} from './js/componentes';
+import './style.css'
+console.log('Ejecutado el index.js');
+const nombre = 'Juan san juan';
+saludar(nombre);
+
+// Probando comentariosgit branch -M main
